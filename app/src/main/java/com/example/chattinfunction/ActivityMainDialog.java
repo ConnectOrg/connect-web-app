d9714befb269170c52156e0541f7411fabc750f2
freakbeast14 <shahilmohammed14@gmail.com>
2021-06-04 22:23:07 +0530

    converted to original

--- app/src/main/java/com/example/chattinfunction/ActivityMainDialog.java
+++ app/src/main/java/com/example/chattinfunction/ActivityMainDialog.java
@@ -21,11 +21,10 @@ import com.stfalcon.chatkit.dialogs.DialogsListAdapter;
 public class ActivityMainDialog extends AppCompatActivity implements DialogsListAdapter.OnDialogClickListener<ModelOFDialog> {
 
     DialogsList dialogsList;
-    ActivityMainDialogAdapter dialogsListAdapter;
+    DialogsListAdapter dialogsListAdapter;
     ImageLoader imageLoadergg;
     ImageView imageView;
     String url;
-    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +38,7 @@ public class ActivityMainDialog extends AppCompatActivity implements DialogsList
     }
 
     private void adapterActivate() {
-        dialogsListAdapter = new ActivityMainDialogAdapter(
+        dialogsListAdapter = new DialogsListAdapter(
                 R.layout.item_view_dialog_gg,
                 ViewHolderDialogActivity.class,
                 imageLoadergg
@@ -54,53 +53,6 @@ public class ActivityMainDialog extends AppCompatActivity implements DialogsList
     @Override
     public void onDialogClick(ModelOFDialog dialog) {
         ActivityMainMessage.start(this);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search_menu, menu);
-
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) menu.findItem(R.id.action_search)
-                .getActionView();
-        searchView.setSearchableInfo(searchManager
-                .getSearchableInfo(getComponentName()));
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                dialogsListAdapter.getFilter().filter(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String query) {
-                dialogsListAdapter.getFilter().filter(query);
-                return false;
-            }
-        });
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_search) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (!searchView.isIconified()) {
-            searchView.setIconified(true);
-            return;
-        }
-        super.onBackPressed();
     }
 }
 
